d9d4e25402f964dfa1eae0f041c266dbe5739e09
Hui-Wei0301 <71656022+Hui-Wei0301@users.noreply.github.com>
2021-07-05 11:31:59 +0100

    Add files via upload

---
+++ embedding.py
@@ -0,0 +1,18 @@
+# on another CPU machine
+from bert_serving.client import BertClient
+import os
+
+bc = BertClient()  # ip address of the GPU machine
+train_path = r'~\huiwei\baseline\text-segmentation-master\data\WikiSection\wikisection_dataset_ref\en_city_train'
+fileList = os.listdir(train_path)
+for file in fileList:
+    f = open(file,'r')
+    sentences = f.readlines()
+    for sentence in sentences:
+        embedding = bc.encode(sentence)
+        with open(r'~\huiwei\bert\bert_emb_train','a+') as f1:
+            f1.write(embedding+'\n')
+    f1.write('\n')
+    f.close()
+f1.close()
+# bc.encode(['First do it', 'then do it right', 'then do it better'])
