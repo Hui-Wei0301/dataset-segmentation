14f51fe252e3a118b1387e433637288c489970f1
Hui-Wei0301 <71656022+Hui-Wei0301@users.noreply.github.com>
2021-07-10 23:17:21 +0100

    Update embedding.py

--- embedding.py
+++ embedding.py
@@ -2,7 +2,7 @@
 from bert_serving.client import BertClient
 import os
 
-bc = BertClient()  # ip address of the GPU machine
+bc = BertClient(check_length=False)  # ip address of the GPU machine
 train_path = r'/home/weihui/huiwei/baseline/text-segmentation-master/data/WikiSection/wikisection_dataset_ref/en_disease_train'
 # train_path = r'D:\dissertation\baseline\text-segmentation-master\data\wikisection\wikisection_dataset_ref\en_city_train'
 fileList = os.listdir(train_path)
